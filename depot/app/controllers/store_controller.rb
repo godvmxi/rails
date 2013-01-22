2f750d4df72f37cea71a14f53e50d8ba86e66a00
godvmxi <godvmxi@gmail.com>
2013-01-22 11:29:55 +0800

    lesson 9

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -8,10 +8,18 @@ class StoreController < ApplicationController
 		@cart = find_cart
 		@cart.add_product(product)
 		redirect_to(:action => 'display_cart')
+	rescue
+	    logger.error("Attempt to access invalid product #{params[:id]}")
+	    flash[:notice] = 'Invalid product'
+	    redirect_to(:action => 'index')
 	end
 	def display_cart
 		@cart = find_cart
 		@items = @cart.items
+		if @items.empty?
+    		flash[:notice] = "Your cart is currently empty"
+   			redirect_to(:action => 'index')
+   		end
 	end
 
 	private
